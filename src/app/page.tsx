f2deb8e77d3ff13bb0ab1d340ed3479d961158dd
Omar – Personal <omar-personal@Omars-MBP-Work.attlocal.net>
2023-09-13 23:17:36 -0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,73 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { Canvas } from "react-three-fiber";
+import { useRef, useState } from "react";
+import { Box, Button } from "rebass";
+import { useRouter } from "next/navigation";
+import Scene from "../components/Scene.js";
 
 export default function Home() {
+  const inputRef = useRef();
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const enterSite = () => [router.push("/music")];
+
+  // set is shop open to open
+
+  // set the query to password field
+  // if correct, redirect to shop page & open shop in context
+
+  //zIndex: 0
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Canvas
+        shadowMap
+        colorManagement
+        gl={{ alpha: false }}
+        camera={{ position: [0, 0, 7], fov: 50 }}
+        style={{
+          position: "fixed",
+          overflow: "hidden",
+          bottom: 0,
+          left: 0,
+          height: "100vh",
+          width: "100%",
+          background: "white",
+        }}
+      >
+        <Scene />
+      </Canvas>
+      <Box
+        sx={{
+          position: "fixed",
+          display: "flex",
+          flexDirection: "column",
+          margin: "auto auto",
+          left: 0,
+          width: "100%",
+          justifyContent: "center",
+          top: ["40vh", "45vh", "50vh"],
+          transition: "all 400ms ease 100ms",
+        }}
+      >
+        <button
+          style={{
+            marginBottom: 0,
+            fontFamily: "Lekton",
+            backgroundColor: "transparent",
+            border: "none",
+            alignSelf: "center",
+            cursor: "pointer",
+            fontSize: 24,
+            textDecoration: "underline",
+          }}
+          onClick={() => enterSite()}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <i>ENTER</i>
+        </button>
+      </Box>
+    </div>
+  );
 }
